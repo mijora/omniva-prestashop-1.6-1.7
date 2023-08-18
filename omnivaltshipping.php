a0f03979908710f9ca7828a2eebcdf519c729ec5
Rimvydas V <lithstud@gmail.com>
2023-08-18 15:16:00 +0300

    v2.0.9 Release ready

--- omnivaltshipping.php
+++ omnivaltshipping.php
@@ -83,7 +83,7 @@ class OmnivaltShipping extends CarrierModule
     {
         $this->name = 'omnivaltshipping';
         $this->tab = 'shipping_logistics';
-        $this->version = '2.0.8';
+        $this->version = '2.0.9';
         $this->author = 'Mijora';
         $this->need_instance = 0;
         $this->ps_versions_compliancy = array('min' => '1.6', 'max' => _PS_VERSION_);
